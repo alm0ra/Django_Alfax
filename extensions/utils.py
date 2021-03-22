c5d132f69befcdff4127c3b2ebaa4dd9e30eb5df
ali <ali.mrd318@gmail.com>
2021-03-22 18:21:32 +0430

    Edit jalali calender Extension add item jday,jmonth

--- extensions/utils.py
+++ extensions/utils.py
@@ -41,3 +41,25 @@ def jalali_converter(time):
     ) 
 
     return persian_number_converter(output)
+
+def jalali_converter_2(time):
+    jmonth = ['فروردین','اردیبهشت','خرداد','تیر','مرداد','شهریور','مهر','آبان','آذر','دی','بهمن','اسفند']
+    time = timezone.localtime(time)
+    time_to_str = "{},{},{}".format(time.year, time.month, time.day)
+    time_to_tuple = jalali.Gregorian(time_to_str).persian_tuple()
+    time_to_list =list(time_to_tuple)
+
+    for index , month in enumerate(jmonth):
+        if time_to_list[1] == index +1 :
+            time_to_list[1]= month
+            break
+
+    output = "{} {} {} , ساعت {}:{}".format(
+        time_to_list[2],
+        time_to_list[1],
+        time_to_list[0],
+        time.hour,
+        time.minute,
+    ) 
+
+    return time_to_list[2], time_to_list[1], time_to_list[0]
