f8f96b890ecbde7e4cd3055e71f2f2e9ca803d3c
ali <ali.mrd318@gmail.com>
2021-03-29 21:34:36 +0430

    some change i dont know but they are after Karonline

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
